28105a7942ebbc540673425352a21073806e4333
Ubuntu <ubuntu@vps-ac34f99b.vps.ovh.net>
2023-05-06 15:45:14 +0000

    mongo connector

--- mongo_connector.py
+++ mongo_connector.py
@@ -4,3 +4,6 @@ import pymongo
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["twitch_clips"]
 CLIPS = db["clips"]
+BROADCASTERS = db["broadcasters"]
+TWITCH_TOKENS = db["twitch_tokens"]
+YT_TOKENS = db["yt_tokens"]
