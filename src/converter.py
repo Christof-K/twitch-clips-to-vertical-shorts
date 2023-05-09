5eb3be51d562b061756cc3cd0b1ba3de4001d409
Krzysztof.K <k.kosciuszko@companion.pl>
2023-05-09 23:51:08 +0800

    converter - background ambient color

--- src/converter.py
+++ src/converter.py
@@ -1,14 +1,16 @@
 import os
 from moviepy.editor import *
-from moviepy.video.fx.all import rotate
 from storage.clip_storage import set_converted, get_clips_to_convert, Clip, set_error
 from dotenv import load_dotenv
 load_dotenv();
+from moviepy.video.fx.all import resize,rotate
+from skimage.filters import gaussian
 
 channel_name = os.environ.get('YT_CHANNEL_NAME')
 converted_folder = "converted"
 temp_folder = "temp"
 max_height = 1080
+
 
 def convert_to_vertical(clip: Clip):
 
@@ -24,15 +26,20 @@ def convert_to_vertical(clip: Clip):
     text = TextClip('twitch.tv/'+clip.broadcaster_name, fontsize=60, color='white', bg_color='transparent', size=(black_bar.size[0], 50))
     text = text.set_position(('center', 50)).set_duration(video_clip.duration)
 
+    # Create a blurred background of the inner clip
+    background_clip = resize(video_clip, newsize=(video_clip.size[0] // 4, video_clip.size[1] // 4))
+    background_clip = background_clip.fl_image(lambda image: gaussian(image.astype(float), sigma=25))
+    background_clip = resize(background_clip, height=1920)
+    background_clip = background_clip.set_position("center")
+
      # Create a semi-transparent watermark text diagonally across the inner clip
-    watermark_text = TextClip(channel_name, fontsize=70, color='white', bg_color='transparent', stroke_width=1)
+    watermark_text = TextClip(channel_name, fontsize=20, color='white', bg_color='transparent', stroke_width=1)
     watermark_text = watermark_text.set_position(("center", "center"), relative=True)
     watermark_text = rotate(watermark_text, angle=-35, unit="deg", resample="bicubic", expand=True)
-    watermark_text.set_opacity(0.55)
     watermark_text = watermark_text.set_duration(video_clip.duration)
 
     # Combine the black bar, centered clip, and text into a final composite clip
-    final_clip = CompositeVideoClip([black_bar, centered_clip, text, watermark_text])
+    final_clip = CompositeVideoClip([black_bar, background_clip, centered_clip, text, watermark_text])
 
     # Write the final clip to a new video file
     final_clip.write_videofile(
